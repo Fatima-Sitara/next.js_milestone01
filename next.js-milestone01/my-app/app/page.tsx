101d69e8fcc70e2e437d0d6c1875b6ddaca5098b
Fatima-Sitara <160725111+Fatima-Sitara@users.noreply.github.com>
2024-11-10 22:13:41 +0500

    Add files via upload

---
+++ next.js-milestone01/my-app/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <center style={{padding:"20px",backgroundColor:"lightblue"}}>
+        <h1>Hello, world!</h1>
+      </center>
+    </div>
+  )
+}
